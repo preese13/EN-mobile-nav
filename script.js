e0aa92ad8e7ca6b89da848d2781a8864b9d5d978
Patrick Reese <preese@employeenavigator.com>
2018-04-02 11:44:27 -0400

    message

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
-
+	var isOpen = false;
+	var productsOpen = false;
 	$('#nav-icon3').click(function(){
 
 		$(this).toggleClass('open');
 		$('.nav-info-container').toggleClass('display');
 		$('.navbar-mobile').toggleClass('navbar-open');
 		window.scrollTo(0, 0);
+
 		
 	});
-	$('#products').click(function(){
-		console.log(1)
-		$('.products-table').toggleClass('display-products');
+
+	$('#products-toggle').click(function(){
+		
+		if(productsOpen) {
+			productsOpen = ! productsOpen;
+			$('.products-info-container').toggleClass('display');
+			setTimeout(function(){ 
+				$('.navbar-mobile').toggleClass('navbar-open');
+				$('#products-row').toggleClass('products-open');
+				$('.mobile-nav-row-2').toggleClass('resize');
+			 }, 440);
+		}
+		else {
+			productsOpen = ! productsOpen;
+			$('.products-info-container').toggleClass('display');
+			$('.navbar-mobile').toggleClass('navbar-open');
+			$('#products-row').toggleClass('products-open');
+			$('.mobile-nav-row-2').toggleClass('resize');	
+		}
 	});
 });
